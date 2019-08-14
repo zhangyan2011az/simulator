c3ae177cfc1c58b72defdd8328bb88422cb3f34c
zhangyanyan <zhangyanyan@163.com>
2019-08-14 23:17:29 +0800

    基本框架

---
+++ simulator/src/main/java/cn/com/crbank/SimulatorApplication.java
@@ -0,0 +1,13 @@
+package cn.com.crbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorApplication.class, args);
+	}
+
+}
